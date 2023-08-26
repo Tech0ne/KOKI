9a37b4a025e862ac7bea37d6047680d1ffc51222
Tech0ne <clement.piasco@epitech.eu>
2023-08-27 01:35:00 +0200

    [INIT] Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+## 
+## Simple tools that uses MITM to grab
+## cookies from any website (by spoofing domain name)
+## 
+## DO NOT USE IN ANYTHING ELSE THAN EDUCATION AND PENTEST
+## I DO NOT TAKE ANY RESPONSABILITY FOR ANY ILLEGAL USE OF
+## THIS TOOL
+## 
+
+import sys
+
+import core
+
+if __name__ == "__main__":
+    sys.exit(core.main(sys.argv))
